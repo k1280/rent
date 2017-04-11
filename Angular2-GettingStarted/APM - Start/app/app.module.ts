224eb33f7ee7495a93e0045f8e7b6c2106925947
Katarzyna Wicherska <kawi@gft.com>
2017-04-11 15:42:52 +0200

    forms, pipe update

--- Angular2-GettingStarted/APM - Start/app/app.module.ts
+++ Angular2-GettingStarted/APM - Start/app/app.module.ts
@@ -1,12 +1,12 @@
 import { ProductListComponent } from './products/product-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent }  from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent, ProductListComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, ProductListComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }

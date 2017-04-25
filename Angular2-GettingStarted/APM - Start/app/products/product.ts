f47bcab5d9521672026248da5d2ca75c6fad6f97
Katarzyna Wicherska <kawi@gft.com>
2017-04-25 14:34:46 +0200

    vv

--- Angular2-GettingStarted/APM - Start/app/products/product.ts
+++ Angular2-GettingStarted/APM - Start/app/products/product.ts
@@ -1,6 +1,6 @@
 export interface IProduct {
     productId: number;
-    productName: number;
+    productName: string;
     productCode: string;
     releaseDate: string;
     description: string;
@@ -9,18 +9,18 @@ export interface IProduct {
     imageUrl: string;
 }
 
-export class Product implements IProduct {
-    constructor(public productId: number,
-        public productName: number,
-        public productCode: string,
-        public releaseDate: string,
-        public description: string,
-        public price: number,
-        public starRating: number,
-        public imageUrl: string) {
-    }
+// export class Product implements IProduct {
+//     constructor(public productId: number,
+//         public productName: number,
+//         public productCode: string,
+//         public releaseDate: string,
+//         public description: string,
+//         public price: number,
+//         public starRating: number,
+//         public imageUrl: string) {
+//     }
 
-    calculateDiscount(percent: number): number {
-        return this.price - (this.price * percent / 100);
-    }
-}
+//     calculateDiscount(percent: number): number {
+//         return this.price - (this.price * percent / 100);
+//     }
+// }

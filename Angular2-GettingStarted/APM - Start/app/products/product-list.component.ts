3eaf8ef45fc28af699e95e9bac6e7ca36925b9ee
Katarzyna Wicherska <kawi@gft.com>
2017-04-26 09:05:57 +0200

    nested components: event emitter, output: displaying an info about clicked stars

--- Angular2-GettingStarted/APM - Start/app/products/product-list.component.ts
+++ Angular2-GettingStarted/APM - Start/app/products/product-list.component.ts
@@ -54,5 +54,9 @@ export class ProductListComponent implements OnInit {
     ngOnInit(): void {
         console.log('In OnInit');
     }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+    }
 }
 

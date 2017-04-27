441eaff07d1f338dd3c76ec2784dc36cba3563da
Katarzyna Wicherska <kawi@gft.com>
2017-04-27 10:17:12 +0200

    Http, Observable, rxjx -> map imported, constructor and private _url

--- Angular2-GettingStarted/APM - Start/app/products/product.service.ts
+++ Angular2-GettingStarted/APM - Start/app/products/product.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 import { IProduct } from './product';
 
+
 @Injectable()
 export class ProductService {
-
+    private _productUrl = 'api/products/products.json'; //here normally would be the url of webserver, now it's just a json with the list of products
+    constructor(private _http: Http) { }
     getProducts(): IProduct[] {
         return [
             {

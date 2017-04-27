9dbf6d25a981f2e73c685c46f2d65d98aa69699b
Katarzyna Wicherska <kawi@gft.com>
2017-04-27 16:16:16 +0200

    addedRouterModule, forRoot and paths in it plus components, TODO welcome component

--- Angular2-GettingStarted/APM - Start/app/app.module.ts
+++ Angular2-GettingStarted/APM - Start/app/app.module.ts
@@ -1,8 +1,11 @@
+import { ProductDetailComponent } from './products/product-detail.component';
 import { ProductListComponent } from './products/product-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { StarComponent } from './shared/star.component';
@@ -10,11 +13,20 @@ import { StarComponent } from './shared/star.component';
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule, 
-    HttpModule
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   declarations: [
     AppComponent,
+    WelcomeComponent,
+    ProductDetailComponent,
     ProductListComponent,
     ProductFilterPipe,
     StarComponent
